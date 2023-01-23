cfa9b02574868d89bb31f892ff72ac41eb7dea91
Ko Htut <kohtut@192.168.1.43>
2023-01-23 18:35:24 +0700

    fix : db config

--- data/conf/db.php
+++ data/conf/db.php
@@ -4,11 +4,11 @@
  */
 return array(
     'DB_TYPE' => 'mysql',
-    'DB_HOST' => '127.0.0.1',
-    'DB_NAME' => '668yuanma.com',
-    'DB_USER' => '668yuanma.com',//源码下载：668yuanma.com
-    'DB_PWD' => '668yuanma.com',
-    'DB_PORT' => '3306',
+    'DB_HOST' => 'yoyodb-do-user-13170287-0.b.db.ondigitalocean.com',
+    'DB_NAME' => 'bet77',
+    'DB_USER' => 'bet77',
+    'DB_PWD' => 'AVNS_2dbHYpCscD515_axUx1',
+    'DB_PORT' => '25060',
     'DB_PREFIX' => 'jz_',
     //密钥
     "AUTHCODE" => '0n8J5h9sfkxofRI9wy',
